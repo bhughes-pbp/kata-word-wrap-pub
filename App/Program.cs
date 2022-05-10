dea29e1b51b58b61bd93662fbf342c0b86cd8d76
Ben Hughes <bhughes@paybyphone.com>
2022-05-10 12:10:42 -0700

    Add sample app and test

---
+++ App/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace App;
+
+public static class Hello
+{
+    public static string Wrap(string input)
+    {
+        return "Hey";
+    }
+}
