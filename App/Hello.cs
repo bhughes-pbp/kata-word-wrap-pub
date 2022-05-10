1f1c4b746955b445097cb21d13b55b0518b27229
Ben Hughes <bhughes@paybyphone.com>
2022-05-10 12:20:17 -0700

    Add sln

---
+++ App/Hello.cs
@@ -0,0 +1,9 @@
+namespace App;
+
+public static class Hello
+{
+    public static string Wrap(string input)
+    {
+        return "Hey";
+    }
+}
--- App/Program.cs
+++ App/Program.cs
@@ -1,10 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-namespace App;
-
-public static class Hello
-{
-    public static string Wrap(string input)
-    {
-        return "Hey";
-    }
-}
+Console.Out.WriteLine("main");
